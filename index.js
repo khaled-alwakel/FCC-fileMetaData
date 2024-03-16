977fda6fe561864fe55ee4a17504ddd987f4308a
Khaled Alwakel <khaledmofreh33@gmail.com>
2024-03-16 00:48:51 +0000

    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+app.use (express.json())
 
+const multer = require('multer');
+const storage = multer.memoryStorage();
+
+
+app.use(express.static('public'));
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -12,7 +18,21 @@ app.get('/', function (req, res) {
 });
 
 
+capp.post('/upload', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
 
+  // Extract file information from req.file
+  const { originalname, mimetype, size } = req.file;
+
+  // Send a JSON response with file information
+  res.json({
+    filename: originalname,
+    type: mimetype,
+    size: size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
