66a3dd62b00ae255cf011e3776beb039596f75f6
Khaled Alwakel <khaledmofreh33@gmail.com>
2024-03-17 01:32:52 +0000

    update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
 });
 
 
-app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: 'No file uploaded' });
   }
