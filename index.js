9ebf339f4d2e2a16299cbd5b5ac4b2d125054990
Khaled Alwakel <khaledmofreh33@gmail.com>
2024-03-17 02:08:36 +0000

    update index.js

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
-const express = require('express');
+const  express = require('express');
+const  cors = require('cors');
+require('dotenv').config()
 const multer = require('multer');
+const upload = multer()
 
-const app = express();
+
+const  app = express();
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  if (!req.file) return res.status(400).json({ error: 'No file uploaded' });
+  
+  return res.status(200).json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
+
 const port = process.env.PORT || 3000;
-
-// Multer configuration for file upload
-const storage = multer.memoryStorage(); // Storing files in memory
-const upload = multer({ storage: storage });
-
-// Serving the HTML page
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-// File upload and metadata route
-app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
-    // Checking if file was uploaded
-    if (!req.file) {
-        return res.status(400).json({ error: 'No file uploaded' });
-    }
-
-    // Sending file information in the response
-    res.json({
-        name: req.file.originalname,
-        type: req.file.mimetype,
-        size: req.file.size
-    });
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
 });
