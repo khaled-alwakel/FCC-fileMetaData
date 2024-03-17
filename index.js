8f793269e42aead0c5725ef99b14c466dbfadbbf
Khaled Alwakel <khaledmofreh33@gmail.com>
2024-03-17 01:09:14 +0000

    update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ c
   });
 });
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
